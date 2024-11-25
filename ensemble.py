030649c0a8dbad6d345aeb1ea1da3f7038182567
Mimis Chlympatsos <mimis.chlympatsos@mail.utoronto.ca>
2024-11-25 15:28:43 -0500

    knn and ensemble

--- ensemble.py
+++ ensemble.py
@@ -1,1 +1,107 @@
-# TODO: complete this file.
+import random 
+import numpy as np 
+from utils import (
+    load_valid_csv,
+    load_public_test_csv,
+    load_train_sparse,
+    sparse_matrix_evaluate,
+)
+from sklearn.impute import KNNImputer
+from knn import knn_impute_by_user
+from item_response import *
+
+def bagging(matrix, n):
+    """
+    Given a matrix where "points" are rows and features are columns,
+    return n matrices produced by sampling from the origin matrix with replacement.
+    """
+    height, width = matrix.shape
+    # For each step, randomly generate a number from 0 to height-1, corresponding to index of the row of `matrix`
+    # that we are going to sample
+    ensemble_indices =  np.array([[random.randint(0, height-1) for _ in range(height)] for _ in range(n)])
+    # `ensemble_indices` is an array of shape (n, 524), where the each row includes the indices of the rows in the 
+    # original matrix `matrix` that were sampled.
+    # Using advanced numpy indexing, for each of the n rows (each row corresponding to a bag) we 
+    # extract the required rows from `matrix`. 
+    ensemble = np.array([matrix[inds] for inds in ensemble_indices])
+    return ensemble
+
+class Learner:
+    """
+    A wrapper for a learner/imputer.
+
+    The reason is that we wanted to try bagging for several different base models, and this provides 
+    a consistent way (a contract) to do this.
+
+    Attributes:
+        - name: the name of the learner (str)
+        - hyper: a dictionary with any hyperparameters needed for the model (dict)
+    """
+
+    def __init__(self, name, hyper):
+        self.name = name
+        self.hyper = hyper
+
+    def learn(self, matrix):
+        """
+        Given the sparse matrix of students as rows and questions as columns, 
+        and using the dictionary attribute of hyperparameters `self.hyper`,
+        return a matrix with the Nans filled.
+        """
+        raise NotImplementedError
+
+
+class KNNLearner(Learner):
+    """
+    Learner implementation of user-based KNN
+    """
+    def __init__(self, k, name="KNN"):
+        hyper = {"k": k}
+        super().__init__(name=name, hyper=hyper)
+    
+    def learn(self, matrix):
+        nbrs = KNNImputer(n_neighbors=self.hyper['k'])
+        mat = nbrs.fit_transform(matrix)
+        return mat
+
+
+def ensemble_predict(ensemble, learner, display=False):
+    pred_mats = np.array([None for _ in range(3)])
+
+    for i in range(len(ensemble)):
+        tsamp = ensemble[i]
+        mat = learner.learn(tsamp)
+        pred_mats[i] = mat
+        if display:
+            singular_acc = sparse_matrix_evaluate(test_data, mat)
+            print(f"singular_acc: {singular_acc}")
+        
+    stacked = np.stack(pred_mats, axis=2)
+    avg = np.mean(stacked, axis=2)
+    avg_rounded = avg.round(decimals=0)
+    
+    return avg_rounded
+
+
+if __name__ == "__main__":
+    sparse_matrix = load_train_sparse("./data").toarray()
+    val_data = load_valid_csv("./data")
+    test_data = load_public_test_csv("./data")
+
+    ensemble = bagging(sparse_matrix, 3)
+    
+    # ---- KNN ----
+    k = 8
+    knn_learner = KNNLearner(k=k)
+
+    # ---- Prob ----
+    ...
+
+    # ---- NN ----
+    ...
+
+    final_mat = ensemble_predict(ensemble, knn_learner, display=True)
+    acc = sparse_matrix_evaluate(test_data, final_mat)
+    print(f"ensemble_acc: {acc}")
+    print(f"original_acc: {sparse_matrix_evaluate(test_data, knn_learner.learn(sparse_matrix))}")    
+
--- knn.py
+++ knn.py
@@ -44,12 +44,62 @@ def knn_impute_by_item(matrix, valid_data, k):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
+    nbrs = KNNImputer(n_neighbors=k)
+
+    ### NOTE
+    # In `knn_impute_by_user`, we assumed that a student's answer to a question
+    # is expected to be similar to the answers of "similar" students to that 
+    # question (where similarity is measured as a Nan-tolerant euclidean distance 
+    # between the answer vectors of students)
+    #
+    # Here, where we perform knn based on question similarity, the underlying assumption is different. 
+    # We assume that if two questions have the same answers from students (that is, is students 
+    # that answered correctly one question also answered the other question correctly, and 
+    # students that answered one question falsely also answered the other question falsely), 
+    # then the answer of some student to the first question should be the same as the 
+    # answer of the SAME student in the other question.
+    ###
+
+    ### NOTE: We assume the inputted matrix has students as rows and questions as columns.
+    # The reason in a matrix, a "point" is conceived to be a row (at least that's how 
+    # scikit treats matrices; the array [[1,2], [4,2], [4,9], [1,10]] has 4 points of 2 
+    # dimensions/features each). 
+    # Assuming it hasn't been mutated before entering this function in any way, the matrix
+    # will have one row for each student and one column for each question. Thus, simply 
+    # taking the transpose of the matrix gives a row for each question and a column for each 
+    # student.
+    # Note how the representation changes. Now each "point" in space is a questions, with 
+    # N-dimensions, N being the number of students. For each student, we have whether 
+    # they answered that question correctly or not.
+    mat_trans = matrix.T
+
+    # We use NaN-Euclidean distance measure.
+    imputed_mat_trans = nbrs.fit_transform(mat_trans)
+
+    # Not that the `sparse_matrix_evaluate` function expects a matrix with shape
+    # (num students)-by-(num-questions), so we take the transpose to return 
+    # to the original representation.
+    imputed_mat = imputed_mat_trans.T
+    acc = sparse_matrix_evaluate(valid_data, imputed_mat)
+    print("Validation Accuracy: {}".format(acc))
+    return acc
     acc = None
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
     return acc
 
+def plot(xs, ys, genlabel, xlabel, ylabel, title, show=True, save=False, filepath="plot.png"):
+    plt.figure(figsize=(10,6))
+    plt.scatter(xs, ys, label = genlabel )
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.legend()
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(filepath) # must be run from within `~/starter` directory
 
 def main():
     sparse_matrix = load_train_sparse("./data").toarray()
@@ -67,7 +117,25 @@ def main():
     # the best performance and report the test accuracy with the        #
     # chosen k*.                                                        #
     #####################################################################
-    pass
+
+    knn_func = knn_impute_by_item 
+    # knn_func = knn_impute_by_user
+
+    k_vals = [1, 6, 11, 16, 21, 26]
+    accs = np.array([knn_func(sparse_matrix, val_data, k) for k in k_vals])
+    k_best = np.argmax(accs)
+
+    filename = "knn-item.png" if knn_func == knn_impute_by_item else "knn-stud.png"
+
+    plot(xs=k_vals, ys=accs, genlabel="k-vs-acc", xlabel="k", ylabel="validation-accuracy", 
+            title="Validation Accuracy for different k", save=True, show=False, filepath=f"./knn-results/{filename}")
+    
+    test_acc = knn_func(sparse_matrix, test_data, k_vals[k_best])
+    best_k_msg = f"The highest performing k is k*={k_vals[k_best]}, with test accuracy {test_acc}"
+    
+    print(best_k_msg)
+
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
