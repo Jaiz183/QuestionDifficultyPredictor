b897a308ca349bd3cabae2c1ea14e3d195ebeb8f
Jaiz183 <jaizepic@gmail.com>
2024-11-27 00:17:48 -0500

    Added template files

---
+++ augmentation.py
@@ -0,0 +1,23 @@
+"""
+Augmentation.
+"""
+import numpy as np
+
+
+def add_metadata() -> np.ndarray:
+    """
+    Returns data matrix with metadata added.
+    """
+    ...
+
+def compute_pcs(k) -> np.ndarray:
+    """
+    Returns first k PCs of data.
+    """
+    ...
+
+def boosted_knn():
+    """
+    Boosted kNN.
+    """
+    ...
